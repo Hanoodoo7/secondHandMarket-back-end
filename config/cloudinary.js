8e152d9124e67a43dafced89146a63abde6a1ce6
Hanoodoo7 <mohalmaskati@gmail.com>
2025-08-13 22:08:15 +0300

    adding cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,10 +1,19 @@
+// config/cloudinary.js
 const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: 'dbtxztzci',
+  api_key: '623986378841871',
+  api_secret: 'bFqd8m-zVkCL56KidLGBmmmTbyo'
 });
 
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'secondhand_market', // Folder name in Cloudinary
+    allowed_formats: ['jpg', 'png', 'jpeg'],
+  },
+});
 
-module.exports = { cloudinary };
+module.exports = { cloudinary, storage };
